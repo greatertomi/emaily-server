9976d6228ed5e29b9856baf373045b398f63a20b
Oshalusi John <oshalusijohn@gmail.com>
2020-09-25 19:19:45 +0100

    Upgraded to async await

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+      '769097688364-4d3e1bp08kqlvh3e7ag37g2ebuq7uk7j.apps.googleusercontent.com',
+    googleClientSecret: 'BzzCCzDVzLXaRXZGJz-RXmzM',
+    mongoURI:
+      'mongodb+srv://john:john@cluster0.n5gne.mongodb.net/Emaily?retryWrites=true&w=majority',
+    cookieKey: 'euepekkdjdhvsbs'
+};
