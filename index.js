062c84c04947bea2502f3825cae5d1e7f4928880
Oshalusi John <oshalusijohn@gmail.com>
2020-10-17 18:05:49 +0100

    Added stripe

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('./models/User');
 require('./services/passport');
 
 const authRoutes = require('./routes/authRoutes');
+const billingRoutes = require('./routes/billingRoutes');
 
 connectDB();
 const app = express();
@@ -21,8 +22,10 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.json());
 
 authRoutes(app);
+billingRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
