414683a86108157e05ed2c0479edb6da934f1e8c
Oshalusi John <oshalusijohn@gmail.com>
2020-10-19 22:23:31 +0100

    Finished working on sending emails

--- config/dev.js
+++ config/dev.js
@@ -10,5 +10,6 @@ module.exports = {
   stripeSecretKey:
     'sk_test_51HVhddL8doPHWczDQ20DYD5nQUx5NXzKnTsHBp31w8AE1Jd5HZRl5aJUClSIskEgGnlzquHlB4PZbo1atAZtBWWX00DO5quSk0',
   sendGridKey:
-    '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'
+    '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',
+  redirectDomain: 'http://localhost:3000'
 };
